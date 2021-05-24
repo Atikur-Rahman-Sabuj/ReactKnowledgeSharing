9ef4311dc4276f12599ade86cb1b3a10be351484
Sabuj-PC-GD <sabuj.kucse@gmail.com>
2021-05-25 01:19:38 +0600

    added raw react

---
+++ rawReact.js
@@ -0,0 +1,19 @@
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = { liked: false };
+   }
+
+   render() {
+      if (this.state.liked) {
+         return "You liked this.";
+      }
+
+      return e("button", { onClick: () => this.setState({ liked: true }) }, e("div", {}, "Like"));
+   }
+}
+
+const domContainer = document.querySelector("#root_raw");
+ReactDOM.render(e(LikeButton), domContainer);
